f2e51eaacbf843ff4c6c681b1e75347e97453386
Elise Erickson <elisece717@gmail.com>
2024-02-12 15:29:54 -0500

    done

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,26 @@
 class Pet:
-    pass
+    def __init__(self, name, pet_type, owner = None):
+        if pet_type in Pet.PET_TYPES:
+            self.pet_type = pet_type
+        else:
+            raise Exception
+        self.name = name
+        self.owner = owner
+        Pet.all.append(self)
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all = []
 
 class Owner:
-    pass
+    def __init__(self, name):
+        self.name = name
+
+    def pets(self):
+        return [pet for pet in Pet.all if pet.owner == self]
+    
+    def add_pet(self, pet):
+        pet.owner = self
+
+    def get_sorted_pets(self):
+        pets = self.pets()
+        pets.sort(key = lambda pet: pet.name)
+        return pets
